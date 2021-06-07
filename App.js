4c11f797814b11a31ad79f6f96bb9f28e172a758
Miguel <miguelcorrear79@gmail.com>
2021-06-07 12:05:12 -0300

    Primeiro commit

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import colors from './src/styles/colors';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>AppRandomTeam</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.forest_greentraditional,
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ src/styles/colors.js
@@ -0,0 +1,12 @@
+export default {
+  forest_greentraditional: '#10451dff',
+  lincoln_green: '#155d27ff',
+  dartmouth_green: '#1a7431ff',
+  forest_green_web: '#208b3aff',
+  green_pigment: '#25a244ff',
+  dark_pastel_green: '#2dc653ff',
+  malachite: '#4ad66dff',
+  light_green: '#6ede8aff',
+  granny_smith_apple: '#92e6a7ff',
+  celadon: '#b7efc5ff',
+  };
